02a984cc863a8ddd619eafc778a0ad39e1d0b72f
Samuel Cáceres <144251952+samuelcaceres@users.noreply.github.com>
2023-09-14 19:40:05 -0400

    Last commit

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
-const palabras = ["APPLE", "BANANA", "CHERRY", "GRAPE", "ORANGE"];
-let palabra = seleccionarPalabraAleatoria();
-let intentos = 6;
+let intentos = 5;
+let palabra;
 
-console.log(palabra);
-const input = document.getElementById("guess-input");
-const button = document.getElementById("guess-button");
+fetch('https://random-word-api.herokuapp.com/word?length=5&lang=en')
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        palabra = response[0].toUpperCase()
+    })
+    .catch(err => console.log(err));
 
-button.addEventListener('click', intentar);
+const button = document.getElementById("intentar");
+button.addEventListener("click", intentar);
 
-function seleccionarPalabraAleatoria() {
-    const indiceAleatorio = Math.floor(Math.random() * palabras.length);
-    return palabras[indiceAleatorio];
+function intentar() {
+    console.log("Intento!")
 }
+
+const input = document.getElementById("entrada");
+const valor = input.value;
 
 function intentar() {
     const INTENTO = leerIntento();
 
-    if (INTENTO === palabra) {
-        console.log("Ganaste");
+    if (!INTENTO) {
         return;
     }
+
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
     
-    for (let i in palabra) {
-        if (INTENTO[i] === palabra[i]) {
-            console.log(INTENTO[i], "VERDE");
-        } else if (palabra.includes(INTENTO[i])) {
-            console.log(INTENTO[i], "AMARILLO");
-        } else {
-            console.log(INTENTO[i], "GRIS");
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if( palabra.includes(INTENTO[i]) ) { 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {      
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
         }
+        ROW.appendChild(SPAN)
     }
-    
+    GRID.appendChild(ROW)
     intentos--;
 
-    if (intentos === 0) {
-        console.log("Perdiste. La palabra correcta era: " + palabra);
+    if (intentos === 0 || INTENTO === palabra) {
+        terminar(INTENTO === palabra ? "<h1>GANASTE!</h1>" : "<h1>PERDISTE!</h1>");
     }
 }
 
-function leerIntento() {
-    let intento = document.getElementById("guess-input").value;
-    intento = intento.toUpperCase();
+function leerIntento(){
+    let intento = document.getElementById("entrada");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
     return intento;
+}
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("entrada");
+    input.disabled = true;
+    button.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
 }
